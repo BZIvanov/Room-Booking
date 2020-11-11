1494e4d9723c7ad173bb47f4832f42d9d1272bc7
BZIvanov <ivanov.biser@gmx.com>
2020-11-11 18:49:53 +0200

    better file formatting

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,11 +1,11 @@
-const authRoutes = require('../routes/auth')
-const destinationRoutes = require('../routes/destination')
-const statsRoutes = require('../routes/stats')
-const ordersRoutes = require('../routes/order')
+const authRoutes = require('../routes/auth');
+const destinationRoutes = require('../routes/destination');
+const statsRoutes = require('../routes/stats');
+const ordersRoutes = require('../routes/order');
 
 module.exports = (app) => {
-  app.use('/auth', authRoutes)
-  app.use('/destination', destinationRoutes)
-  app.use('/stats', statsRoutes)
-  app.use('/orders', ordersRoutes)
-}
+  app.use('/auth', authRoutes);
+  app.use('/destination', destinationRoutes);
+  app.use('/stats', statsRoutes);
+  app.use('/orders', ordersRoutes);
+};
--- server/routes/order.js
+++ server/routes/order.js
@@ -1,97 +1,91 @@
-const express = require('express')
-const authCheck = require('../config/auth-check')
-const Order = require('../models/Order')
+const express = require('express');
+const authCheck = require('../config/auth-check');
+const Order = require('../models/Order');
 
-const router = new express.Router()
+const router = new express.Router();
 
 router.post('/submit', authCheck, (req, res) => {
-  const products = req.body
+  const products = req.body;
   let orderObj = {
     creator: req.user._id,
-    products
-  }
+    products,
+  };
 
-  Order
-    .create(orderObj)
+  Order.create(orderObj)
     .then((createdOrder) => {
       res.status(200).json({
         success: true,
         message: 'Order created successfully.',
-        data: createdOrder
-      })
+        data: createdOrder,
+      });
     })
     .catch((err) => {
-      console.log(err)
-      const message = 'Something went wrong :('
+      console.log(err);
+      const message = 'Something went wrong :(';
       return res.status(200).json({
         success: false,
-        message: message
-      })
-    })
-})
+        message: message,
+      });
+    });
+});
 
 router.get('/user', authCheck, (req, res) => {
-  Order
-    .find({creator: req.user._id})
-    .then(orders => {
-      res.status(200).json(orders)
-    })
-})
+  Order.find({ creator: req.user._id }).then((orders) => {
+    res.status(200).json(orders);
+  });
+});
 
 router.get('/pending', authCheck, (req, res) => {
   if (req.user.roles.indexOf('Admin') > -1) {
-    Order
-      .find({status: 'Pending'})
-      .then(orders => {
-        res.status(200).json(orders)
-      })
+    Order.find({ status: 'Pending' }).then((orders) => {
+      res.status(200).json(orders);
+    });
   } else {
     return res.status(200).json({
       success: false,
-      message: 'Invalid credentials!'
-    })
+      message: 'Invalid credentials!',
+    });
   }
-})
+});
 
 router.post('/approve/:id', authCheck, (req, res) => {
-  const orderId = req.params.id
-  Order
-    .findById(orderId)
-    .then(order => {
+  const orderId = req.params.id;
+  Order.findById(orderId)
+    .then((order) => {
       if (!order) {
-        const message = 'Order not found.'
+        const message = 'Order not found.';
         return res.status(200).json({
           success: false,
-          message: message
-        })
+          message: message,
+        });
       }
 
-      order.status = 'Approved'
+      order.status = 'Approved';
       order
         .save()
         .then(() => {
           res.status(200).json({
             success: true,
-            message: 'Order approved successfully.'
-          })
+            message: 'Order approved successfully.',
+          });
         })
         .catch((err) => {
-          console.log(err)
-          const message = 'Something went wrong :('
+          console.log(err);
+          const message = 'Something went wrong :(';
           return res.status(200).json({
             success: false,
-            message: message
-          })
-        })
+            message: message,
+          });
+        });
     })
     .catch((err) => {
-      console.log(err)
-      const message = 'Something went wrong :('
+      console.log(err);
+      const message = 'Something went wrong :(';
       return res.status(200).json({
         success: false,
-        message: message
-      })
-    })
-})
+        message: message,
+      });
+    });
+});
 
-module.exports = router
+module.exports = router;
