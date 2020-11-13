e37447ce42d1e4a42fc68d4152044aeb7fcee906
BZIvanov <ivanov.biser@gmx.com>
2020-11-13 18:31:27 +0200

    moved logout and user-nav components in respective folders

--- frontend/src/components/atoms/index.js
+++ frontend/src/components/atoms/index.js
@@ -1,4 +1,5 @@
 export { default as Link } from './link';
 export { default as NavLink } from './nav-link';
+export { default as Logout } from './logout';
 export { default as Footer } from './footer';
 export { default as NotFound } from './not-found';
