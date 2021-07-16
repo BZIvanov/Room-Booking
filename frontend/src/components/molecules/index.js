a686d85a9f6519d0cfdc3cee9443687a3ce04b1f
BZIvanov <ivanov.biser@gmx.com>
2021-07-16 20:22:56 +0300

    removed the simple project

--- frontend/src/components/molecules/index.js
+++
@@ -1,2 +0,0 @@
-export { default as UserNav } from './user-nav';
-export { default as ProfileDestinations } from './profile-destinations';
--- server/utilities/error-handler.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  handleMongooseError: (err) => {
-    const key = Object.keys(err.errors)[0];
-    return err.errors[key].message;
-  },
-};
