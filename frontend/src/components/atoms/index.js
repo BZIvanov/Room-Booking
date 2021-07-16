ca31da5d5b5b721dd19aa924e015b2f4c33d2480
BZIvanov <biser.z.ivanov@gmail.com>
2021-07-16 20:22:56 +0300

    removed the simple project

--- frontend/src/components/atoms/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Link } from './link';
-export { default as NavLink } from './nav-link';
-export { default as Logout } from './logout';
-export { default as Footer } from './footer';
-export { default as NotFound } from './not-found';
