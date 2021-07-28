1ac088e0032152cb62355d25f1efba8b2a096229
BZIvanov <ivanov.biser@gmx.com>
2021-07-28 19:12:24 +0300

    error handling

---
+++ middlewares/catch-async.js
@@ -0,0 +1,7 @@
+export default (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    next(error);
+  }
+};
--- pages/api/rooms/index.js
+++ pages/api/rooms/index.js
@@ -1,8 +1,9 @@
 import nc from 'next-connect';
 import dbConnect from '../../../config/dbConnect';
 import { getAllRooms, createRoom } from '../../../controllers/rooms';
+import onError from '../../../middlewares/global-error';
 
-const handler = nc();
+const handler = nc({ onError });
 
 dbConnect();
 
